6f9574f95a439016c74d740192a38b80b7ab6930
nhtam <nht9499@gmail.com>
2023-02-24 10:10:44 +0700

    update new api

--- functions/src/modules/similar/similar.service.ts
+++ functions/src/modules/similar/similar.service.ts
@@ -60,96 +60,90 @@ export class SimilarService {
       if (!system) return;
       templateList = system.data.map((file) => file.url);
     }
-    testList.map(async (test) => {
-      let res: any = {};
-      if (type === 'internet') {
-        const payload = {
-          user: userUid,
-          testFile: test,
-          number_of_keyword: numberOfKeyword,
-          number_of_result: numberOfResult,
-        };
-        res = await axios.post(
-          'http://127.0.0.1:8000/api/search_on_internet/',
-          payload
-        );
-      } else {
-        const payload = {
-          user: userUid,
-          testFile: test,
-          templateFile: templateList,
-        };
-        res = await axios.post(
-          'http://127.0.0.1:8000/api/compute_create/',
-          payload
-        );
-      }
-      if (!res.data)
-        throw new CustomServerError({ message: 'server/internal' });
-      // const baseDir = '/Users/tamnguyen/Downloads';
-      const baseDir = `D:/DATN/datn-web-app/public`;
-      const testFilePath = `${baseDir}/${generateNanoid()}.json`;
-      const templateFilePath = `${baseDir}/${generateNanoid()}.json`;
-      fs.writeFile(testFilePath, JSON.stringify(res.data.testFile), (err) => {
-        if (err) throw err;
-      });
-      fs.writeFile(
-        templateFilePath,
-        JSON.stringify(res.data.templateFile),
-        (err) => {
-          if (err) throw err;
-        }
-      );
-      await this.saleStatisticsByYearRepository.updateStatistics({
-        monthId: formatDateToMonthId(new Date()),
-        fileCount: 0,
-        newUserCount: 0,
-        testCount: 1,
-      });
-      const uid = generateNanoid();
-      const schema: SimilarSchema = {
-        uid,
-        createdAt: now,
-        updatedAt: now,
-        userUid,
-        organizationSnapshot: null,
-        dataTestUrl: testFilePath,
-        dataTemplateUrl: templateFilePath,
-        status: 'done',
-        matchPercent: res.data.matchPercent ?? 0,
-        type,
-        language: [],
+    let res: any = {};
+    if (type === 'internet') {
+      const payload = {
+        user: userUid,
+        testFile: testList,
+        number_of_keyword: numberOfKeyword,
+        number_of_result: numberOfResult,
       };
-      if (type === 'organization' && organizationUid) {
-        const organization = await this.organizationRepository.getOrganization(
-          organizationUid
-        );
-        if (!organization) return;
-        schema.organizationSnapshot = {
-          uid: organization.uid,
-          name: organization.fullName,
-        };
-      }
-      res.data.testFile.map((file: any) => {
-        file.info.map((info: any) => {
-          if (!schema.language.includes(info.type)) {
-            schema.language.push(info.type);
-          }
-        });
-      });
-      await this.testRepository.createTestResult(schema);
-      if (sendEmail) {
-        if (count === testList.length - 1) {
-          await this.emailService.sendEmail({
-            to: user.email ?? 'nht9499@gmail.com',
-            subject: `Kết quả kiểm tra tương đồng ${formatDateToString(
-              new Date()
-            )}`,
-            html: `Kết quả kiểm tra của bạn đã sẵn sàng hãy click <a href="http://localhost:3001/histories/${uid}" style="text-decoration: none;"> vào đây</a> để xem`,
-          });
-        }
-        count++;
-      }
+      res = await axios.post(
+        'http://127.0.0.1:8000/api/search_on_internet/',
+        payload
+      );
+    } else {
+      const payload = {
+        user: userUid,
+        threshold: 0.8,
+        testFile: testList,
+        templateFile: templateList,
+      };
+      res = await axios.post('http://127.0.0.1:8000/api/computes/', payload);
+    }
+    if (!res.data) throw new CustomServerError({ message: 'server/internal' });
+
+    // TODO: change baseDir for mac
+    // const baseDir = '/Users/tamnguyen/Downloads';
+    const baseDir = `D:/DATN/datn-web-app/public`;
+    const testFilePath = `${baseDir}/${generateNanoid()}.json`;
+    const templateFilePath = `${baseDir}/${generateNanoid()}.json`;
+    fs.writeFile(testFilePath, JSON.stringify(res.data.testFile), (err) => {
+      if (err) throw err;
     });
+    fs.writeFile(
+      templateFilePath,
+      JSON.stringify(res.data.templateFile),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    await this.saleStatisticsByYearRepository.updateStatistics({
+      monthId: formatDateToMonthId(new Date()),
+      fileCount: 0,
+      newUserCount: 0,
+      testCount: 1,
+    });
+    const uid = generateNanoid();
+    const schema: SimilarSchema = {
+      uid,
+      createdAt: now,
+      updatedAt: now,
+      userUid,
+      organizationSnapshot: null,
+      dataTestUrl: testFilePath,
+      dataTemplateUrl: templateFilePath,
+      status: 'done',
+      matchPercent: res.data.matchPercent ?? 0,
+      type,
+      language: [],
+    };
+    if (type === 'organization' && organizationUid) {
+      const organization = await this.organizationRepository.getOrganization(
+        organizationUid
+      );
+      if (!organization) return;
+      schema.organizationSnapshot = {
+        uid: organization.uid,
+        name: organization.fullName,
+      };
+    }
+    res.data.testFile.map((file: any) => {
+      file.info.map((info: any) => {
+        if (!schema.language.includes(info.type)) {
+          schema.language.push(info.type);
+        }
+      });
+    });
+    await this.testRepository.createTestResult(schema);
+    if (sendEmail) {
+      await this.emailService.sendEmail({
+        to: user.email ?? 'nht9499@gmail.com',
+        subject: `Kết quả kiểm tra tương đồng ${formatDateToString(
+          new Date()
+        )}`,
+        html: `Kết quả kiểm tra của bạn đã sẵn sàng hãy click <a href="http://localhost:3001/histories/${uid}" style="text-decoration: none;"> vào đây</a> để xem`,
+      });
+    }
   }
 }
